6ec995a190d0bc1b4cf2ce603aa97ee200c35f86
David Kohler <dkohlkat@gmail.com>
2023-11-01 23:01:12 -0400

    Initial Commit

---
+++ Appointment Scheduler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appointment Scheduler
+//
+//  Created by Sean Alan on 11/1/23.
+//  Copyright © 2023 Bleepy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
